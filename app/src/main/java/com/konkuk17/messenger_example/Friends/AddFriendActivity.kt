3bf34004b43fc2ac81ecd1c1329ae087e21c784c
hongmon1 <hongmon1@naver.com>
2021-10-16 15:16:24 +0900

    친구추가, 검색 액티비티 추가

---
+++ app/src/main/java/com/konkuk17/messenger_example/Friends/AddFriendActivity.kt
@@ -0,0 +1,12 @@
+package com.konkuk17.messenger_example.Friends
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.konkuk17.messenger_example.R
+
+class AddFriendActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_friend)
+    }
+}
