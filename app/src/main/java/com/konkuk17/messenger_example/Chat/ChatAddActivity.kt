ed192d2643c2729b0cfba327a14f35766527580b
YoumHyeJi <yheji0321@gmail.com>
2021-10-25 17:32:23 +0900

    채팅 추가

--- app/src/main/java/com/konkuk17/messenger_example/Chat/ChatAddActivity.kt
+++ app/src/main/java/com/konkuk17/messenger_example/Chat/ChatAddActivity.kt
@@ -1,17 +1,25 @@
 package com.konkuk17.messenger_example.Chat
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.konkuk17.messenger_example.Friends.FriendRecycleViewAdapter
 import com.konkuk17.messenger_example.Friends.FriendRecycleViewData
 import com.konkuk17.messenger_example.R
 import com.konkuk17.messenger_example.databinding.ActivityChatAddBinding
+import com.konkuk17.messenger_example.databinding.FriendrecycleItemInChattingBinding
 
 class ChatAddActivity : AppCompatActivity() {
-    lateinit var binding : ActivityChatAddBinding
-    var selectedFriend : String? = null
-    var friendList : ArrayList<FriendRecycleViewData>? = null
+    lateinit var binding: ActivityChatAddBinding
+    var selectedFriend: String? = null
+    var friendList: ArrayList<FriendRecycleViewData>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,15 +28,71 @@ class ChatAddActivity : AppCompatActivity() {
         init()
     }
 
-    fun init(){
-        friendList= intent.getSerializableExtra("friendList") as ArrayList<FriendRecycleViewData>
+    fun init() {
+        friendList = intent.getSerializableExtra("friendList") as ArrayList<FriendRecycleViewData>
 
-        if(friendList != null){
-            Log.d("chat add activity", "size : " + friendList?.size)
-            for (f in friendList!!){
-                Log.d("chat add activity", "name : " + f?.name)
+        if (friendList != null) {
+
+            var myAdapter = FriendRVAdapter(friendList!!)
+            myAdapter.listener = object : FriendRVAdapter.onFriendSelectedListener {
+                override fun onFriendSelected(myfriend: FriendRecycleViewData) {
+                    var alertDialog = AlertDialog.Builder(this@ChatAddActivity)
+                        .setTitle("채팅방 생성")
+                        .setMessage(myfriend.name + "님과 채팅방을 생성하시겠습니까?")
+                        .setNegativeButton("취소", DialogInterface.OnClickListener { dialog, which ->
+                            Log.d("채팅방 생성", "채팅방 생성을 취소했습니다.")
+                            Toast.makeText(this, "채팅방 생성을 취소했습니다.", Toast.LENGTH_SHORT)
+                        })
+                        .setPositiveButton("확인", DialogInterface.OnClickListener { dialog, which ->
+                            Log.d("채팅방 생성", "채팅방을 생성했습니다.")
+                            Toast.makeText(this, "채팅방을 생성했습니다.", Toast.LENGTH_SHORT)
+                        })
+                    alertDialog.show()
+                }
             }
-            //binding.chataddRecyclerview.adapter = FriendRecycleViewAdapter(this, friendList, )
+
+            binding.chataddRecyclerview.apply {
+                adapter = myAdapter
+                layoutManager = LinearLayoutManager(this@ChatAddActivity, LinearLayoutManager.VERTICAL, false)
+            }
+
         }
     }
+
 }
+
+
+class FriendRVAdapter(var friendList: ArrayList<FriendRecycleViewData>) :
+    RecyclerView.Adapter<FriendRVAdapter.MyViewHolder>() {
+
+    var listener: onFriendSelectedListener? = null
+
+    interface onFriendSelectedListener {
+        fun onFriendSelected(myfriend: FriendRecycleViewData)
+    }
+
+    inner class MyViewHolder(var itemBinding: FriendrecycleItemInChattingBinding) :
+        RecyclerView.ViewHolder(itemBinding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        var itemBinding =
+            FriendrecycleItemInChattingBinding.inflate(LayoutInflater.from(parent.context))
+        return MyViewHolder(itemBinding)
+
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.itemBinding.friendNameInChatting.text = friendList[position].name
+        holder.itemBinding.friendInChatLayout.setOnClickListener {
+            listener?.onFriendSelected(friendList[position])
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return friendList.size
+    }
+
+}
+
