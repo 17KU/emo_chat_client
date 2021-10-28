1d410bc66a52b08b66ad11b0cbab566ba734588a
YoumHyeJi <yheji0321@gmail.com>
2021-10-28 19:21:45 +0900

    수정

--- app/src/main/java/com/konkuk17/messenger_example/LoadingActivity.kt
+++ app/src/main/java/com/konkuk17/messenger_example/LoadingActivity.kt
@@ -1,7 +1,10 @@
 package com.konkuk17.messenger_example
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.konkuk17.messenger_example.Login.LoginActivity
 import com.konkuk17.messenger_example.databinding.ActivityLoadingBinding
 
 class LoadingActivity : AppCompatActivity() {
@@ -10,5 +13,18 @@ class LoadingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoadingBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        startLoading()
     }
+
+    fun startLoading(){
+        var handler : Handler = Handler()
+        handler.postDelayed(object : Runnable{
+            override fun run() {
+                var myintent = Intent(this@LoadingActivity, LoginActivity::class.java)
+                startActivity(myintent)
+                finish()
+            }
+        }, 2000)
+    }
+
 }
